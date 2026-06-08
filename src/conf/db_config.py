06db42e42ee66899e830a49a2e3ed216fcadaf85
yangwanqiu <yangwanqiu@chuanglintech.com>
2026-06-08 14:54:46 +0800

    解决merge问题

--- src/conf/db_config.py
+++ src/conf/db_config.py
@@ -3,37 +3,17 @@
 数据库连接配置
 从 utils/config_loader 获取配置
 """
-import sys
-import os
-
-# 添加项目根目录到路径
-def _add_project_root():
-    current_file = os.path.abspath(__file__)
-    # src/conf/db_config.py -> 向上三级到项目根目录
-    project_root = os.path.dirname(os.path.dirname(os.path.dirname(current_file)))
-    if project_root not in sys.path:
-        sys.path.insert(0, project_root)
-
-_add_project_root()
-
 import pymysql
 from src.utils.config_loader import get_database_config, get_main_config, ConfigError
 
 DB_CONFIG = get_database_config()
 
-# 可选配置，如果配置
-def _safe_get_config(key, default=None):
-    try:
-        return get_main_config(key)
-    except:
-        return default
+KIMI_API_KEY = get_main_config('kimi.api_key')
+KIMI_BASE_URL = get_main_config('kimi.base_url')
+KIMI_MODEL = get_main_config('kimi.model')
 
-KIMI_API_KEY = _safe_get_config('kimi.api_key')
-KIMI_BASE_URL = _safe_get_config('kimi.base_url')
-KIMI_MODEL = _safe_get_config('kimi.model')
-
-DASHSCOPE_API_KEY = _safe_get_config('dashscope.api_key')
-QWEN_MODEL = _safe_get_config('qwen.model')
+DASHSCOPE_API_KEY = get_main_config('dashscope.api_key')
+QWEN_MODEL = get_main_config('qwen.model')
 
 
 def get_connection():
